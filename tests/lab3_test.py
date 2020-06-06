787787be1f5228dc9203229915b21df30b88063c
j.gosztyla <goszjacek@gmail.com>
2020-06-06 22:12:41 +0200

    add bool

--- tests/lab3_test.py
+++ tests/lab3_test.py
@@ -11,4 +11,4 @@ test("int a = 5", "[('DEC', ('a', 'int', ('INT', 5)))]")
 
 test("a += 5","[('INCREMENT', ('a', ('INT', 5)))]")
 
-test("true", "[('BOOL', true)]")
+test("true", "[('BOOL', True)]")
